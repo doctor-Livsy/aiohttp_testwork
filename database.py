69254519ede7785ae7ccc7b03e33861fab674cf5
dr.Livsy <plumbus.rise@gmail.com>
2022-07-25 14:20:45 +0300

    Database and raw api

---
+++ database.py
@@ -0,0 +1,80 @@
+from typing import Optional
+
+import asyncio
+import asyncpg
+
+
+class Database:
+
+    def __init__(self, config: dict) -> None:
+        self.config: dict = config
+        self.connection_pool: Optional[asyncpg.Pool] = None
+
+    @staticmethod
+    def format_args(sql, parameters: dict) -> tuple:
+        sql += " AND ".join([f"{item} = ${num}" for num, item in enumerate(parameters, start=1)])
+        return sql, tuple(parameters.values())
+
+    @staticmethod
+    def args_to_string(*args) -> str:
+        string = ''
+        for arg in args:
+            string += arg + ', '
+        return string[:-2]
+
+    @staticmethod
+    def parameters_to_string(dictionary: dict) -> str:
+        string = ''
+        for key, value in zip(dictionary.keys(), dictionary.values()):
+            string += f'{key} = {value}, '
+
+        return string[:-2]
+
+    async def create_users_table(self) -> None:
+        request = """
+                CREATE TABLE IF NOT EXISTS users(
+                ID  SERIAL PRIMARY KEY,
+                user_name VARCHAR(255),
+                password VARCHAR(255),
+                UNIQUE(user_name))
+                """
+
+        async with self.connection_pool.acquire() as connection:
+            await connection.execute(request)
+
+    async def create_images_table(self) -> None:
+        request = """
+                CREATE TABLE IF NOT EXISTS images(
+                ID  SERIAL PRIMARY KEY,
+                image_id VARCHAR(255),
+                image BYTEA,
+                UNIQUE(image_id))
+                """
+
+        async with self.connection_pool.acquire() as connection:
+            await connection.execute(request)
+
+    async def check_user_exist(self, user_name: str):
+        request = f"SELECT * FROM users WHERE user_name=$1"
+        return await self.connection_pool.fetchrow(request, user_name)
+
+    async def add_user(self, user_name: str, password: str) -> bool:
+        request = "INSERT INTO users(user_name, password) VALUES ($1, $2)"
+        async with self.connection_pool.acquire() as connection:
+            user = await self.check_user_exist(user_name=user_name)
+            if user is None:
+                await connection.execute(request, user_name, password)
+                return True
+        return False
+
+    async def run_database(self) -> None:
+        self.connection_pool = await asyncpg.create_pool(
+            database=self.config['POSTGRES_DB'],
+            user=self.config['POSTGRES_USER'],
+            password=self.config['POSTGRES_PASSWORD'],
+            host=self.config['POSTGRES_HOST']
+        )
+
+        await self.create_users_table()
+        await self.create_images_table()
+
--- server.py
+++ server.py
@@ -1,33 +1,100 @@
+import json
+import logging
+import jwt
+
 from aiohttp import web
+from datetime import datetime, timedelta
+from database import Database
+
+
+JWT_SECRET = 'super_secured_secret'
+JWT_ALGORITHM = 'HS256'
+JWT_EXP_DELTA_SECONDS = 30
+
+# jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
+
+
+def create_access_token(payload: dict) -> dict:
+    jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
+    return {'access_token': jwt_token}
 
 
 class AsyncServer:
 
     def __init__(self, config: dict, app: web.Application) -> None:
-        self.server_config: dict = config['server_config']
-        self.app: web.Application = app
+        self._server_config: dict = config['server_config']
+        self._app: web.Application = app
+        self._database: Database = Database(config=config['database_config'])
+        self._logger: logging.Logger = logging.getLogger(__name__)
 
-    async def _on_startup(self) -> None:
+    @staticmethod
+    def _get_payload(data: dict) -> dict:
+        payload = {
+            'user_id': data['id'],
+            'expiration': datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS)
+        }
+        return payload
+
+    async def _register_user(self, request: web.Request) -> web.Response:
+        data: dict = await request.json()
+        user = data['user_name']
+
+        add_user_result = await self._database.add_user(**data)
+        if add_user_result:
+            return web.json_response(text=f'User "{user}" has been successfully registered')
+        else:
+            return web.json_response(text=f'User "{user}" already exist')
+
+    async def _authenticate(self, request: web.Request) -> web.Response:
+        data: dict = await request.json()
+        user = data['user_name']
+        password = data['password']
+
+        result_db = await self._database.check_user_exist(user)
+        if result_db and result_db['user_name'] == user and result_db['password'] == password:
+
+            result = {
+                'ok': 200,
+                'access_token': 'fasrgat43q23424rt34tergferg',
+            }
+            # TODO: вставить функцию выдачи токена
+            return web.json_response(text=json.dumps(result))
+        else:
+            return web.json_response(text=f'Unknown user "{user}"')
+
+    async def _download_info(self, request) -> web.Response:
+        return web.json_response(data={'ok': 200})
+
+    async def _get_info(self, request) -> web.Response:
+        return web.json_response(data={'ok': 200})
+
+    async def _on_startup(self, app: web.Application) -> None:
         # asyncpg create pool
-        pass
+        await self._database.run_database()
 
-    async def _on_shutdown(self) -> None:
+    async def _on_shutdown(self, app: web.Application) -> None:
         # Close connections pool
-        pass
+        await self._database.connection_pool.close()
+
+    def _init_handlers(self) -> None:
+        self._app.router.add_post('/auth', self._authenticate)
+        self._app.router.add_post('/register_user', self._register_user)
+        self._app.router.add_post('/api/download_info', self._download_info)
+        self._app.router.add_get('/api/get_info', self._get_info)
 
     def _run(self) -> None:
         web.run_app(
-            app=self.app,
-            host=self.server_config['host'],
-            port=self.server_config['port']
+            app=self._app,
+            host=self._server_config['host'],
+            port=self._server_config['port']
         )
 
     def run(self) -> None:
         # setup routes
-        print('')
+        self._init_handlers()
 
         # init callbacks
-        self.app.on_startup.append(self._on_startup)
-        self.app.on_shutdown.append(self._on_shutdown)
+        self._app.on_startup.append(self._on_startup)
+        self._app.on_shutdown.append(self._on_shutdown)
 
         self._run()
